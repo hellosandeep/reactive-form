0844515c69d164fdf070a42a5f6351291bbd177f
aboutclinic <aboutclinic.com@gmail.com>
2019-09-05 14:23:23 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+/**
+ * Import only If use form control
+ */
+import { FormControl } from '@angular/forms';
+/**
+ * Import only If use form group
+ */
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-form';
+  title = 'Reactive Form';
+
+  users: any = [];
+
+  /**
+   * Reactive form control WITHOUT group. uncomment below two line
+   */
+
+  // name: new FormControl(''),
+  // address: new FormControl('')
+
+  /**
+   * Reactive form control WITH group. uncomment below two line
+   */
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    address: new FormControl('')
+  })
+
+  constructor() {
+  }
+
+  save() {
+
+    /**
+     * Get form control value  WITHOUT group
+     * console.log(this.name.value, this.address.value)
+     */
+
+    /**
+    * Get form control value  WITHOUT group
+    * console.log(this.profileForm.value)
+    */
+    console.log(this.profileForm.value)
+    this.users.push(this.profileForm.value);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/**
+ * START
+ * Angular Reactive Form Module.
+ * import only if want to use Reactive Form.
+ */
+import { ReactiveFormsModule } from '@angular/forms';
+/**
+ * START
+ * Material Module.
+ * import only if want to use material
+ */
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+//END
 
 @NgModule({
   declarations: [
@@ -10,7 +25,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
